7b38b1b3089fcfa38841d4a973af6b9870c3c6fe
Josh Wuebben <jwuebben@precisionplanting.com>
2024-01-28 13:06:27 -0600

    add copilot

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -68,6 +68,34 @@ local plugins = {
     enabled = false,
   },
 
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "copilot",  group_index = 2 },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+    },
+  },
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
